69b5aa2099f478bc83a247e78aac34a297d2f5f9
ankit <ansingh16@gmail.com>
2026-06-03 22:53:11 +0100

    fix: remove eager imports from __init__ to avoid pulling matplotlib on cloud

--- src/uk_road_safety/__init__.py
+++ src/uk_road_safety/__init__.py
@@ -1,8 +1,3 @@
-from uk_road_safety.data import load_dft_data, preprocess_features
-from uk_road_safety.evaluate import find_best_models, optimize_threshold
-from uk_road_safety.models import get_sampling_techniques, train_models
-from uk_road_safety.predict import load_model_artifacts, predict_severity
-
 __all__ = [
     "load_dft_data",
     "preprocess_features",
